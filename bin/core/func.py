f5327d7a414563d13ac04d839286a2009cef3e62
boot1110001 <boot111.0001@gmail.com>
2018-12-28 17:56:23 +0100

    mod imports and functions

--- bin/core/func.py
+++ bin/core/func.py
@@ -6,7 +6,9 @@ from os import listdir
 from os.path import isfile, join
 
 ### FUNCTIONS ##################################################################
-def list_lvls():
+def get_lvls():
     onlyfiles = [f for f in listdir('lvls') if isfile(join('lvls', f))]
-    for onefile in onlyfiles:
-        print(' '+onefile)
+    return onlyfiles
+
+def draw_map(womap):
+    for o in womap: o.draw()
